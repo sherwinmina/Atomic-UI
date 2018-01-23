af23a307a943203057462642143e5d72f6cb78a3
Sherwin Mina <sherwinmina@yahoo.com>
2018-01-23 15:37:33 -0800

    first commit

---
+++ src/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import styles from './App.scss'
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.App}>
+        <header className={styles.header}>Hello</header>
+        <p className="App-intro">hello</p>
+      </div>
+    )
+  }
+}
+
+export default App
