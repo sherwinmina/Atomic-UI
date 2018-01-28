710f5b512c8250c09f830553363bceb21be32573
Sherwin Mina <sherwinmina@yahoo.com>
2018-01-28 10:45:10 -0800

    Added input

---
+++ src/atomic-ui/Input/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Proptypes from 'prop-types'
+
+class Input extends Component {
+  render() {
+    return (
+      <div>
+        <Input type="text" />
+      </div>
+    )
+  }
+}
+
+export default Input
