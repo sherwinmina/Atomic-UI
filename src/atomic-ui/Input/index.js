bb931884845a98a621713088582a8217a175bd4f
Sherwin Mina <sherwinmina@yahoo.com>
2018-01-28 11:18:17 -0800

    Added proptypes and scss

--- src/atomic-ui/Input/index.js
+++ src/atomic-ui/Input/index.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react'
 import Proptypes from 'prop-types'
 
 class Input extends Component {
+  state = {}
+  static PropTypes = {
+    title= Proptypes.string
+  }
+
   render() {
     return (
       <div>
-        <Input type="text" />
+        <div title={this.props.title}></div>
+        <Input className='input' type="text" />
       </div>
     )
   }
